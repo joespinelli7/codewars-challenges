ca219e7910f9727f6d245ad88f70f10b9779a1a8
github username <josephspinelli@yahoo.com>
2019-09-24 21:42:56 -0500

    iqtest set up

---
+++ iq-test/index.js
@@ -0,0 +1,11 @@
+// Write a program that among the given numbers  finds one that is different in evenness, and return a position
+// of this number. Keep in mind that your task is to help solve a real IQ test, which means indexes of the elements
+// start from 1 (not 0).
+// Examples:
+// iqTest("2 4 7 8 10") => 3 // Third number is odd, while the rest of the numbers are even
+// iqTest("1 2 1 1") => 2 // Second number is even, while the rest of the numbers are odd
+​
+
+function iqTest(numbers){
+  
+}
