86c9fe5a915d41358c18415e9d1db270cfe040b2
github username <josephspinelli@yahoo.com>
2019-09-30 15:45:19 -0500

    set up

---
+++ the-wave/index.js
@@ -0,0 +1,10 @@
+// Your task is to create a function that turns a string into a Mexican Wave. You will be
+// passed a string and you must return that string in an array where an uppercase letter is a person standing up.
+// The input string will always be lower case but maybe empty. If the character in the string is whitespace then
+// pass over it as if it was an empty seat.
+// Example:
+// wave("hello") => ["Hello", "hEllo", "heLlo", "helLo", "hellO"]
+
+function wave(str){
+  
+}
