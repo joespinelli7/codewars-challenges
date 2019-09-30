3a41d7c805ecfa467029ab248a86b0a8d2864345
github username <josephspinelli@yahoo.com>
2019-09-30 16:47:28 -0500

    done

--- the-wave/index.js
+++ the-wave/index.js
@@ -6,5 +6,19 @@
 // wave("hello") => ["Hello", "hEllo", "heLlo", "helLo", "hellO"]
 
 function wave(str){
-  
+  const waveArr = [];
+
+  for (let i = 0; i < str.length; i++) {
+    newStr = str.slice(0, i) + str.charAt(i).toUpperCase() + str.slice(i + 1)
+    waveArr.push(newStr);
+  }
+
+  // removes element if it matches the exact string passed in b/c then I know it's where a space occurs and
+  // don't want to include it in the array. Otherwise, no spaces present so just return the first wave array.
+  if (waveArr.includes(str)) {
+    const finalWaveArr = waveArr.filter((element) => element !== str);
+    return finalWaveArr;
+  } else {
+    return waveArr;
+  }
 }
