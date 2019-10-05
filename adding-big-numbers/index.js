1dab751de6342bae10c98e7cb4044235136747d8
github username <josephspinelli@yahoo.com>
2019-10-04 20:51:55 -0500

    set up

---
+++ adding-big-numbers/index.js
@@ -0,0 +1,9 @@
+// Write a function that returns the sum of two numbers. The input numbers are strings and the function must return
+// a string.
+// Example
+// add("123", "321"); -> "444"
+// add("11", "99"); -> "110"
+
+function add(a, b) {
+  
+}
