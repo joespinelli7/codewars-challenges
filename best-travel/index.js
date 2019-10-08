d4ab8412cbb8e260e60970fe29ab0cf32b9bd85b
github username <josephspinelli@yahoo.com>
2019-10-07 21:23:18 -0500

    set up

--- best-travel/index.js
+++ best-travel/index.js
@@ -1,12 +1,12 @@
 // The function chooseBestSum will take as parameters t (maximum sum of distances, integer >= 0), k (number of
-// towns to visit, k >= 1) and ls (list of distances, all distances are positive or null integers and this list has
-// at least one element). The function returns the "best" sum ie the biggest possible sum of k distances less than
-// or equal to the given limit t, if that sum exists, or otherwise null.
+// towns to visit, k >= 1) and ls (list of distances, all distances are positive or null integers and this list
+// has at least one element). The function returns the "best" sum ie the biggest possible sum of k distances less
+// than or equal to the given limit t, if that sum exists, or otherwise null.
 // Examples:
 // ts = [50, 55, 56, 57, 58] chooseBestSum(163, 3, ts) => 163
 // xs = [50] chooseBestSum(163, 3, xs) => null
 // ys = [91, 74, 73, 85, 73, 81, 87] chooseBestSum(230, 3, ys) => 228
 
 function chooseBestSum(t, k, ls) {
-  
+
 }
