1a7495045c2122899b1e38ccc31f2926d7e3be32
github username <josephspinelli@yahoo.com>
2019-10-11 14:23:05 -0500

    done

--- find-anagrams/index.js
+++ find-anagrams/index.js
@@ -1,4 +1,4 @@
-// Write a function that will find all the anagrams of a word from a list. You will be given two inputs a word and 
+// Write a function that will find all the anagrams of a word from a list. You will be given two inputs a word and
 // an array with words. You should return an array of all the anagrams or an empty array if there are none.
 // Examples:
 // anagrams('abba', ['aabb', 'abcd', 'bbaa', 'dada']) => ['aabb', 'bbaa']
@@ -6,5 +6,18 @@
 // anagrams('laser', ['lazing', 'lazy',  'lacer']) => []
 
 function anagrams(word, words) {
+  const anagramArr = [];
+  const mainWord = cleanStr(word);
 
+  for (let str of words) {
+    if (cleanStr(str) === mainWord) {
+      anagramArr.push(str);
+    }
+  }
+
+  return anagramArr;
+}
+
+function cleanStr(str) {
+  return str.replace(/[^\w]/g, '').toLowerCase().split('').sort().join('');
 }
