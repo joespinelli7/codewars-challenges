befcf2c6b0286c76847b26828fbc8003c6aa2fb9
github username <josephspinelli@yahoo.com>
2019-10-27 21:44:26 -0500

    set up

---
+++ pentabonacci/index.js
@@ -0,0 +1,10 @@
+// Your task is to give the number of total values for the odd terms of the sequence up to the n-th term (included).
+// (The number n (of n-th term) will be given as a positive integer). The values 1 (one) is the only that is
+// duplicated in the sequence and should be counted only once.\
+// Examples:
+// count_odd_pentaFib(5) => 1, because the terms up to 5 are: 0, 1, 1, 2, 4, 8 (only 1 is odd and counted once)
+// count_odd_pentaFib(10) => 3
+
+function countOddPentaFib(n) {
+  
+}
