9a99ceddbbf5acb6b31f8294442bf6e48782e4e0
github username <josephspinelli@yahoo.com>
2019-10-28 22:49:56 -0500

    closer

--- pentabonacci/index.js
+++ pentabonacci/index.js
@@ -9,8 +9,8 @@
 // (The number n (of n-th term) will be given as a positive integer). The values 1 (one) is the only that is
 // duplicated in the sequence and should be counted only once.\
 // Examples:
-// count_odd_pentaFib(5) => 1, because the terms up to 5 are: 0, 1, 1, 2, 4, 8 (only 1 is odd and counted once)
-// count_odd_pentaFib(10) => 3
+// countOddPentaFib(5) => 1, because the terms up to 5 are: 0, 1, 1, 2, 4, 8 (only 1 is odd and counted once)
+// countOddPentaFib(10) => 3
 
 function countOddPentaFib(n) {
   const fibArr = [0, 1];
