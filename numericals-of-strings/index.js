baca8640b85250e7ec15e3c809ddd6cf270b4a0d
github username <josephspinelli@yahoo.com>
2019-11-01 22:08:21 -0500

    set up

---
+++ numericals-of-strings/index.js
@@ -0,0 +1,9 @@
+// Given an input string. For each symbol in the string if it's the first character occurence, replace it with a
+// '1', else replace it with the amount of times you've already seen it.
+// Examples:
+// numericals("Hello, World!") => "1112111121311"
+// numericals("aaaaaaaaaaaa") => "123456789101112"
+
+function numericals(s){
+  
+}
