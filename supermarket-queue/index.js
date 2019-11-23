5b8b06047d8d224bcd396e08af79e36ad3151718
github username <josephspinelli@yahoo.com>
2019-11-23 16:23:59 -0600

    set up

---
+++ supermarket-queue/index.js
@@ -0,0 +1,23 @@
+// There is a queue for the self-checkout tills at the supermarket. Your task is write a function to calculate the
+// total time required for all the customers to check out!
+// Input
+// customers: an array of positive integers representing the queue. Each integer represents a customer, and its
+// value is the amount of time they require to check out.
+// n: a positive integer, the number of checkout tills.
+// Output
+// The function should return an integer, the total time required.
+// Clarifications
+// There is only ONE queue serving many tills, and
+// The order of the queue NEVER changes, and
+// The front person in the queue (i.e. the first element in the array/list) proceeds to a till as soon as it becomes free.
+// Examples:
+// queueTime([5,3,4], 1) => 12
+// should return 12 because when there is 1 till, the total time is just the sum of the times
+// queueTime([10,2,3,3], 2) => 10
+// should return 10 because here n=2 and the 2nd, 3rd, and 4th people in the  queue finish before the 1st person has finished.
+// queueTime([2,3,10], 2) => 12
+// should return 12
+
+function queueTime(customers, n) {
+  
+}
