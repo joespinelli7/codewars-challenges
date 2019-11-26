d15be2f7578cb94af4b6fed8f47eaddf5643df0f
github username <josephspinelli@yahoo.com>
2019-11-25 23:22:52 -0600

    done

--- most-valuable-character/index.js
+++ most-valuable-character/index.js
@@ -1,12 +1,23 @@
-You will be given a string and your task is to return the most valuable character. The value of a character is
-the difference between the index of its last occurrence and the index of its first occurrence. Return the
-character that has the highest value. If there is a tie, return the lexicographically lowest character.
-All inputs will be lower case.
-Example:
-solve('a') => 'a'
-solve('ab') => 'a'. Last occurrence is equal to first occurrence of each character. Return lexicographically lowest.
-solve("axyzxyz") => 'x'
+// You will be given a string and your task is to return the most valuable character. The value of a character is
+// the difference between the index of its last occurrence and the index of its first occurrence. Return the
+// character that has the highest value. If there is a tie, return the lexicographically lowest character.
+// All inputs will be lower case.
+// Example:
+// solve('a') => 'a'
+// solve('ab') => 'a'. Last occurrence is equal to first occurrence of each character. Return lexicographically lowest.
+// solve("axyzxyz") => 'x'
 
 function mvc(str) {
-  
+  let highestValue = 0;
+  let mostValChar = '';
+
+  for (let char of str) {
+    let difference = str.lastIndexOf(char) - str.indexOf(char)
+    if (difference > highestValue) {
+      mostValChar = char;
+      highestValue = difference;
+    }
+  }
+
+  return mostValChar;
 }
